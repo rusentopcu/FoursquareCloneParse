4e3879ed79eb46d08e93ccad6d44ead22e26030e
Rusen Topcu <rusentopcu@msn.com>
2020-03-10 22:19:14 +0300

    Project created

---
+++ FoursquareParse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoursquareParse
+//
+//  Created by Rusen Topcu on 10.03.2020.
+//  Copyright © 2020 Rusen Topcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
