e70c1ef15529ff6d559b2660517982729a86011c
Rusen Topcu <rusentopcu@msn.com>
2020-03-10 23:03:17 +0300

    Data saved & Pull data from ParseDb(Sample)

--- FoursquareParse/AppDelegate.swift
+++ FoursquareParse/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let configuration = ParseClientConfiguration { (ParseMutableClientConfiguration) in
+            ParseMutableClientConfiguration.applicationId = "xmgrBgPNlM3uqpCg98RqcJQJqoDiYiVjJaUcdCzC"
+            ParseMutableClientConfiguration.clientKey = "Xjz0QDTsGxsejzigQcnItGUCcPWQRDhxuladKHya"
+            ParseMutableClientConfiguration.server = "https://parseapi.back4app.com/"
+        }
+        
+        Parse.initialize(with: configuration)
         return true
     }
 
--- FoursquareParse/ViewController.swift
+++ FoursquareParse/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //MARK: - Veri Kaydetmek
+        /*
+        let parseObject = PFObject(className: "Fruits")
+        parseObject["name"] = "Banana"
+        parseObject["calories"] = 150
+        parseObject.saveInBackground { (success, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            }
+            else {
+                print("uploaded")
+            }
+        }
+        */
+        
+        //MARK: - Veri Çekmek
+        let query = PFQuery(className: "Fruits")
+        //query.whereKey("name", equalTo: "Apple")
+        //query.whereKey("calories", greaterThan: 120)
+        query.findObjectsInBackground { (objects, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                print(objects)
+            }
+        }
     }
 
 
